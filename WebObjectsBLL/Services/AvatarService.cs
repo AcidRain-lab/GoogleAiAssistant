0017e182127f0f26354e5b8b3db7bf387212b2ba
TheDigitalRoninAlex <alex.ark@thedigitalronin.com>
2024-12-13 21:04:37 +0200

    add avatar media phones countries

---
+++ WebObjectsBLL/Services/AvatarService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using WebObjectsBLL.DTO;
+
+namespace WebObjectsBLL.Services
+{
+    public class AvatarService
+    {
+        private readonly BankContext _context;
+        private readonly IMapper _mapper;
+
+        public AvatarService(BankContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<AvatarDTO> GetByIdAsync(Guid associatedRecordId)
+        {
+            var avatar = await _context.Avatars.FindAsync(associatedRecordId);
+            return _mapper.Map<AvatarDTO>(avatar);
+        }
+
+        public async Task CreateOrUpdateAsync(AvatarDTO dto)
+        {
+            var avatar = await _context.Avatars.FindAsync(dto.AssociatedRecordId);
+            if (avatar == null)
+            {
+                avatar = _mapper.Map<Avatar>(dto);
+                _context.Avatars.Add(avatar);
+            }
+            else
+            {
+                _mapper.Map(dto, avatar);
+                _context.Avatars.Update(avatar);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid associatedRecordId)
+        {
+            var avatar = await _context.Avatars.FindAsync(associatedRecordId);
+            if (avatar != null)
+            {
+                _context.Avatars.Remove(avatar);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ WebObjectsBLL/Services/MediaDataService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using WebObjectsBLL.DTO;
+
+namespace WebObjectsBLL.Services
+{
+    public class MediaDataService
+    {
+        private readonly BankContext _context;
+        private readonly IMapper _mapper;
+
+        public MediaDataService(BankContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<MediaDataDTO>> GetAllAsync()
+        {
+            var mediaData = await _context.MediaData.ToListAsync();
+            return _mapper.Map<IEnumerable<MediaDataDTO>>(mediaData);
+        }
+
+        public async Task<MediaDataDTO> GetByIdAsync(Guid id)
+        {
+            var mediaData = await _context.MediaData.FindAsync(id);
+            return _mapper.Map<MediaDataDTO>(mediaData);
+        }
+
+        public async Task CreateAsync(MediaDataDTO dto)
+        {
+            var mediaData = _mapper.Map<MediaDatum>(dto);
+            _context.MediaData.Add(mediaData);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(MediaDataDTO dto)
+        {
+            var mediaData = _mapper.Map<MediaDatum>(dto);
+            _context.MediaData.Update(mediaData);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var mediaData = await _context.MediaData.FindAsync(id);
+            if (mediaData != null)
+            {
+                _context.MediaData.Remove(mediaData);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
